16dbd459ffb771ee6478237daf8698a728ecd773
alexstride <stridealexander@gmail.com>
2017-10-13 15:14:10 +0100

    adding seed quiz

--- rails_api/db/seeds.rb
+++ rails_api/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Quiz.create(name: 'My Big Fat Seedy Quiz', image: "http://images.pitchero.com/ui/547726/image_5831c5c0706b9.jpg", creator: "Alex", description: "A really good quiz with really good stuff in it!!")
