f2b69ce056e8e1264c162661ebeda6f9e230a625
alexstride <stridealexander@gmail.com>
2017-10-13 14:48:41 +0100

    adding angular router

---
+++ angular-app/src/js/config/router.js
@@ -0,0 +1,31 @@
+angular
+  .module('hackathon')
+  .config(Router);
+
+Router.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+function Router($stateProvider, $urlRouterProvider, $locationProvider) {
+  $locationProvider.html5mode(true);
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'js/views/home.html'
+    })
+    .state('quizzesIndex', {
+      url: '/quizzes',
+      templateUrl: 'js/views/quizzesIndex.html',
+      controller: 'QuizzesIndexCtrl as quizzesIndex'
+    })
+    .state('quizzesShow', {
+      url: '/quizzes/:id',
+      templateUrl: 'js/views/quizzesShow.html',
+      controller: 'QuizzesShowCtrl as quizzesShow'
+    })
+    .state('quizzesEdit', {
+      url: '/quizzes/:id/edit',
+      templateUrl: 'js/views/quizzesEdit.html',
+      controller: 'QuizzesEditCtrl as quizzesEdit'
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
