982fb609528f1dbf81b7b83fc5cd80ef629f1274
benlloydjones <benlloydjones@gmail.com>
2017-10-13 16:29:43 +0100

    getting questions in controller

--- angular-app/src/js/controllers/quiz.js
+++ angular-app/src/js/controllers/quiz.js
@@ -14,3 +14,32 @@ function QuizzesShowCtrl(Quiz, $state) {
   const vm = this;
   vm.quiz = Quiz.get($state.params);
 }
+
+QuizzesEditCtrl.$inject = ['$state', 'jeopardy', '$scope', 'Quiz'];
+function QuizzesEditCtrl($state, jeopardy, $scope, Quiz) {
+  const vm = this;
+  vm.quiz = Quiz.get($state.params);
+  vm.offset = 0;
+  vm.questions = [];
+
+  $scope.$watch('vm.parameters', () => {
+    vm.offset = 0;
+  }, true);
+
+  function questionButton() {
+    if(vm.offest === 0) vm.questions = [];
+    getQuestions(vm.parameters.category, vm.parameters.value, vm.offset);
+    vm.offset += 5;
+  }
+
+  vm.questionButton = questionButton;
+
+  function getQuestions(category, value, offset) {
+    jeopardy.getQuestions(category, value, offset)
+      .then(responses => {
+        for(let i = 0; i < 5; i++) {
+          vm.questions.unshift(responses[i]);
+        }
+      });
+  }
+}
