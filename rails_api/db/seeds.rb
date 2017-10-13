15d9464fbed834c5428ca7a4683a3078fb50e48e
benlloydjones <benlloydjones@gmail.com>
2017-10-13 15:23:34 +0100

    fixed seeds file

--- rails_api/db/seeds.rb
+++ rails_api/db/seeds.rb
@@ -5,5 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+[Quiz, Question].each do |model|
+  ActiveRecord::Base.connection.execute("TRUNCATE #{model.table_name} RESTART IDENTITY CASCADE")
+end
 
-Quiz.create(name: 'My Big Fat Seedy Quiz', image: "http://images.pitchero.com/ui/547726/image_5831c5c0706b9.jpg", creator: "Alex", description: "A really good quiz with really good stuff in it!!")
+[{
+  name: 'My Big Fat Seedy Quiz',
+  image: "http://images.pitchero.com/ui/547726/image_5831c5c0706b9.jpg",
+  creator: "Alex",
+  description: "A really good quiz with really good stuff in it!!"
+}].each do |quiz|
+  Quiz.create!(quiz)
+end
+
+[{
+  jeopardy_id: 1000000,
+  question: "Who wears the snazziest t-shirt?",
+  answer: "Señor Bradley",
+  value: 100
+}].each do |question|
+  Question.create!(question)
+end
