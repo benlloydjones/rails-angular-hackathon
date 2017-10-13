4af8273add68e6ac44a4f5c1797c8e3d568e9eac
benlloydjones <benlloydjones@gmail.com>
2017-10-13 15:53:57 +0100

    quizzes show controller

--- angular-app/src/js/controllers/quiz.js
+++ angular-app/src/js/controllers/quiz.js
@@ -1,9 +1,16 @@
 angular
   .module('hackathon')
-  .controller('QuizzesIndexCtrl', QuizzesIndexCtrl);
+  .controller('QuizzesIndexCtrl', QuizzesIndexCtrl)
+  .controller('QuizzesShowCtrl', QuizzesShowCtrl);
 
 QuizzesIndexCtrl.$inject = ['Quiz'];
 function QuizzesIndexCtrl(Quiz) {
   const vm = this;
   vm.all = Quiz.query();
+}
+
+QuizzesShowCtrl.$inject = ['Quiz', '$state'];
+function QuizzesShowCtrl(Quiz, $state) {
+  const vm = this;
+  vm.quiz = Quiz.get($state.params);
 }
